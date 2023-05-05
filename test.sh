8e28ea6c9fd44e71271a0156abde28359f45b78c
James Frank <james.frank@publicissapient.com>
2023-05-05 15:00:16 -0400

    (#12) Check for closes #n
    Checks for closes/fixes/resolves/touches #n
    Also adds test action
    Bumps workflow version to v2
    
    Closes #12

--- test.sh
+++ test.sh
@@ -1,17 +1,38 @@
 #!/usr/bin/env bash
 
 commitlintcmd=$(which commitlint)
+testStatus=0
 
 if [ $? != 0 ]; then
-  echo "Please Install commitlint"
+  echo "Please install commitlint"
   exit 1
 fi
 
-for f in ./tests/*.txt
+for f in ./tests/positive*.txt
 do
-  cat "$f" | $commitlintcmd -g ./.github/workflows/commitlint.config.js
+  echo "=========================================="
+  cat "$f" | $commitlintcmd -q -g ./.github/workflows/commitlint.config.js
   if [ $? != 0 ]; then
-    echo "Test $f failed."
+    testStatus=1
+    echo "Failed: Test $f produced a failing commitlint (unexpected)."
+    cat "$f" | $commitlintcmd -V -g ./.github/workflows/commitlint.config.js
+  else
+    echo "Passed: Test $f"
   fi
 done
 
+for f in ./tests/negative*.txt
+do
+  echo "=========================================="
+  cat "$f" | $commitlintcmd -q -g ./.github/workflows/commitlint.config.js
+  if [ $? == 0 ]; then
+    testStatus=1
+    echo "Failed: Test $f produced a passing commitlint (unexpected)."
+    cat "$f" | $commitlintcmd -V -g ./.github/workflows/commitlint.config.js
+    echo "and after"
+  else
+    echo "Passed: Test $f"
+  fi
+done
+
+exit $testStatus
